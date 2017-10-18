2ff89b1aa9ee3b96812fbe8d619fbe51b6732360
zhe.ma <mailMazhe@163.com>
2017-10-18 22:54:32 +0800

    Implement ParabolaEquation.

--- collection/LearnDLL/src/test/main.cpp
+++ collection/LearnDLL/src/test/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <string>
 
 // 1.三阶行列式
@@ -60,6 +61,8 @@ public:
   }
 };
 
+static const std::string kError = "The equation is wrong!";
+
 // 一元二次方程，也就是抛物线方程: y = a·x^2 + b·x + c
 // 导数方程，也就是斜率方程: y' = 2·a·x + b
 class ParabolaEquation {
@@ -70,6 +73,18 @@ public:
 
   bool is_parabola() const {
     return is_parabola_;
+  }
+
+  double a() const {
+    return a_;
+  }
+
+  double b() const {
+    return b_;
+  }
+
+  double c() const {
+    return c_;
   }
 
   // 输入x, 输出对应y。
@@ -83,7 +98,11 @@ public:
   std::string ToSlopeEquationString() const;
 
 private:
+  ParabolaEquation();
+
   void CalculateWeights(const Point& point1, const Point& point2, const Point& point3);
+
+  void CheckParabolaValid();
 
 private:
   double a_;
@@ -93,13 +112,140 @@ private:
   bool is_parabola_;
 };
 
-int main() {
-  ThreeOrderDeterminant s1(1, 3, 1,
-                           2, 1, 1,
-                           -2, 2, -1);
+ParabolaEquation::ParabolaEquation(double a, double b, double c)
+    : a_(a)
+    , b_(b)
+    , c_(c)
+    , is_parabola_(true) {
+  CheckParabolaValid();
+}
 
-  std::cout << s1.CalculateValue() << std::endl;
-  std::cout << s1.ToString() << std::endl;
+ParabolaEquation::ParabolaEquation(const Point& point1, const Point& point2, const Point& point3)
+    : is_parabola_(true) {
+  CalculateWeights(point1, point2, point3);
+}
+
+// Given point1(x1, y1), point2(x2, y2), point3(x3, y3),
+// we can get three equations.
+// y1 = a·x1^2 + b·x1 + c
+// y2 = a·x2^2 + b·x2 + c
+// y3 = a·x3^2 + b·x3 + c
+// And then we can use Cramer's rule to calculate the weights a, b, c.
+void ParabolaEquation::CalculateWeights(const Point& point1, const Point& point2, const Point& point3) {
+  double x1 = point1.x, x2 = point2.x, x3 = point3.x;
+  double y1 = point1.y, y2 = point2.y, y3 = point3.y;
+
+  double x1_2 = x1 * x1, x2_2 = x2 * x2, x3_2 = x3 * x3;
+
+  ThreeOrderDeterminant D(x1_2, x1, 1,
+                          x2_2, x2, 1,
+                          x3_2, x3, 1);
+
+  double d = D.CalculateValue();
+  if (d == 0) {
+    is_parabola_ = false;
+    return;
+  }
+
+  ThreeOrderDeterminant Da(y1, x1, 1,
+                           y2, x2, 1,
+                           y3, x3, 1);
+  double da = Da.CalculateValue();
+  if (da == 0) {
+    is_parabola_ = false;
+    return;
+  }
+
+  a_ = da / d;
+
+  ThreeOrderDeterminant Db(x1_2, y1, 1,
+                           x2_2, y2, 1,
+                           x3_2, y3, 1);
+  b_ = Db.CalculateValue() / d;
+
+  ThreeOrderDeterminant Dc(x1_2, x1, y1,
+                           x2_2, x2, y2,
+                           x3_2, x3, y3);
+  c_ = Dc.CalculateValue() / d;
+}
+
+void ParabolaEquation::CheckParabolaValid() {
+  is_parabola_ = (a_ != 0);
+}
+
+double ParabolaEquation::OutputY(double x) const {
+  if (!is_parabola_) {
+    return 0.0;
+  }
+
+  double y = a_ * x * x + b_ * x + c_;
+  return y;
+}
+
+double ParabolaEquation::OutputSlope(double x) const {
+  if (!is_parabola_) {
+    return 0.0;
+  }
+
+  double y = 2 * a_ * x + b_;
+  return y;
+}
+
+std::string ParabolaEquation::ToParabolaEquationString() const {
+  if (!is_parabola_) {
+    return kError;
+  }
+
+  std::ostringstream sstr;
+  sstr << "y = " << a_ << "x^2";
+
+  if (b_ != 0) {
+    sstr << " + " << b_ << "x";
+  }
+
+  if (c_ != 0) {
+    sstr << " + " << c_;
+  }
+
+  return sstr.str();
+}
+
+std::string ParabolaEquation::ToSlopeEquationString() const {
+  if (!is_parabola_) {
+    return kError;
+  }
+
+  double a = a_ * 2;
+
+  std::ostringstream sstr;
+  sstr << "y = " << a << "x";
+
+  if (b_ != 0) {
+    sstr << " + " << b_;
+  }
+
+  return sstr.str();
+}
+
+int main() {
+  //ThreeOrderDeterminant s1(1, 3, 1,
+  //                         2, 1, 1,
+  //                         -2, 2, -1);
+
+  //std::cout << s1.CalculateValue() << std::endl;
+  //std::cout << s1.ToString() << std::endl;
+
+  ParabolaEquation equation1(Point(0, 1.1), Point(1, 6), Point(2, 17));
+  if (equation1.is_parabola()) {
+    std::cout << equation1.ToParabolaEquationString() << std::endl;
+    std::cout << equation1.ToSlopeEquationString() << std::endl;
+  }
+
+  ParabolaEquation equation2(Point(1, 6), Point(2, 24), Point(3, 54));
+  if (equation2.is_parabola()) {
+    std::cout << equation2.ToParabolaEquationString() << std::endl;
+    std::cout << equation2.ToSlopeEquationString() << std::endl;
+  }
 
   return 0;
 }
